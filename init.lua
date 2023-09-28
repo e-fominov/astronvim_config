30725aafdc0bc6913bebaba62fda15e20e1faee8
Evgeniy <evgeniy.fm@gmail.com>
2023-09-28 13:35:26 +0300

    gruvbox-flat-darg

--- init.lua
+++ init.lua
@@ -23,7 +23,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  colorscheme = "gruvbox-flat",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -16,6 +16,13 @@ return {
       })
     end,
   },
+  {
+      "eddyekofo94/gruvbox-flat.nvim",
+      config = function()
+          vim.o.background = "dark"
+          vim.g.gruvbox_flat_style = "hard"
+      end,
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
